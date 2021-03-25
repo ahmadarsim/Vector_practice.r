ce826bfda1445add29321818552efaca6adf7398
Ahmad Arsim <79629827+ahmadarsim@users.noreply.github.com>
2021-03-25 23:25:20 +0500

    vector.r
    In this file, we discuss the vector.

---
+++ vector.r
@@ -0,0 +1,16 @@
+a<- 6
+ a <- c( "FALSE ", "TRUE")
+  a <- 1:25
+  class(a)
+  
+  a <- c( 8.6 , FALSE)
+  class(a)
+  
+  a <- c( FALSE , TRUE)
+   class(a)
+
+a <- c( "FALSE" , "a")
+  
+   class(a)
+ a <- c(6 , "a" )
+ class(a)
